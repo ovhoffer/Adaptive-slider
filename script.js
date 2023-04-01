c58cd4d7f1d27c427d54f4fb74c93827e4e2e4f7
ovhoffer <karilanko@wp.pl>
2023-04-01 22:55:18 +0200

    trying to add swipe

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const buttonPrev = document.querySelector('.buttons__left');
 const buttonNext = document.querySelector('.buttons__right');
 let width = document.querySelector('.slider__item').clientWidth;
 let sliderWidth = document.querySelector('.slider__container').clientWidth;
-
+const sliderContent = document.querySelector('.slider__container');
 
 
 class Slider {
@@ -42,11 +42,41 @@ window.addEventListener('resize', (e) => {
 buttonNext.onclick = () => slider.next();
 buttonPrev.onclick = () => slider.previous();
 
-let slider = new Slider(width)
+let slider = new Slider(width);
 
 function setSize() {
    sliderList.style.marginLeft = 0 + 'px'
    width = document.querySelector('.slider__item').clientWidth;
    sliderWidth = document.querySelector('.slider__container').clientWidth; 
    slider = new Slider(width);
+}
+
+
+
+
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+
+sliderContent.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+sliderContent.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false); 
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+       slider.previous();
+    }
+    
+    if (touchendX > touchstartX) {
+       slider.next();
+    }
 }
