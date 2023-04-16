9bc691ea51cb49b5beceb9fc520d29e1cf547614
ovhoffer <karilanko@wp.pl>
2023-04-16 23:45:35 +0200

    chanched some architecture of slider

--- script.js
+++ script.js
@@ -9,131 +9,147 @@ const sliderItems = Array.from(document.querySelectorAll('.slider__item'));
 
 
 
-function setIndicator(index) {
-   for (let a = 0; a < sliderIndicators.length; a++) {
-      sliderIndicators[a].classList.remove('indicator__active')
-   }
-   for (let i = 0; i < sliderIndicators.length; i++) {
-      if (i === index) {
-         sliderIndicators[i].classList.add('indicator__active')
-      }
-   }
-}
-
-let n = 0;
-let m = 0;
-
-
-function getEvents() {
-   for (let i = 0; i < sliderIndicators.length; i++) {
-      sliderIndicators[i].addEventListener('click', () => {
-         slider.position = -i * width;
-         slider.newPosition(i);
-         n = i;
-      })
-   }
-}
-
-
-
-
-
-function setPrevNum() {
-   if (n <= 0 || n > sliderItems.length - 1) {
-      n = 0;
-   }
-   else {
-      n--;
-   }
-   return n
-}
-
-function setNextNum() {
-   if (n < 0 || n >= sliderItems.length - 1) {
-      n = 0;
-   }
-   else {
-      n++;
-   }
- return n
-}
-
 
 
 class Slider {
    constructor(stepWidth = 0) {
+      this.n = 0;
       this.position = 0;
       this.step = 0;
       this.stepWidth = stepWidth;
+      this.touchstartX = 0;
+      this.touchstartY = 0;
+      this.touchendX = 0;
+      this.touchendY = 0;
+   }
+
+   init() {
+      buttonNext.onclick = () => this.next();
+      buttonPrev.onclick = () => this.previous();
+      window.addEventListener('resize', setSize)
+      document.addEventListener('DOMContentLoaded', getEvents);
+      // select main track of slider
+      this.sliderList = document.querySelector('.slider__list');
+      // select controllers
+      this.buttonPrev = document.querySelector('.buttons__left');
+      this.buttonNext = document.querySelector('.buttons__right');
+      // select width of slider
+      this.width = document.querySelector('.slider__item').clientWidth;
+      this.sliderWidth = document.querySelector('.slider__container').clientWidth;
+      this.sliderContent = document.querySelector('.slider__container');
+      this.sliderIndicators = Array.from(document.querySelectorAll('.indicator__item')); 
+      this.sliderItems = Array.from(document.querySelectorAll('.slider__item'));
    }
    
    previous() {
       this.position += this.stepWidth;
       this.step--;
-      this.newPosition(setPrevNum());
+      this.newPosition(this.setPrevNum());
    }
 
    next() {
       this.position -= this.stepWidth;
       this.step++;
-      this.newPosition(setNextNum());
+      this.newPosition(this.setNextNum());
    }
 
+   setIndicator(index) {
+      for (let a = 0; a < this.sliderIndicators.length; a++) {
+         this.sliderIndicators[a].classList.remove('indicator__active')
+      }
+      for (let i = 0; i < this.sliderIndicators.length; i++) {
+         if (i === index) {
+            this.sliderIndicators[i].classList.add('indicator__active')
+         }
+      }
+   }
+
+   
+   setPrevNum() {
+      if (this.n <= 0 || this.n > this.sliderItems.length - 1) {
+         this.n = 0;
+      }
+      else {
+         this.n--;
+      }
+      
+      return this.n
+   }
+
+   setNextNum() {
+      if (this.n < 0 || this.n >= this.sliderItems.length - 1) {
+         this.n = 0;
+      }
+      else {
+         this.n++;
+      }
+      return this.n
+      }
+
+   
    newPosition(n) {
       let index = n;
-      if (this.position > 0 || this.position < -(sliderItems.length - 1) * this.stepWidth) {
+      if (this.position > 0 || this.position < -(this.sliderItems.length - 1) * this.stepWidth) {
          this.position = 0;
       }
-      sliderList.style.marginLeft = this.position + 'px';
-      setIndicator(index)
+      this.sliderList.style.marginLeft = this.position + 'px';
+      this.setIndicator(index)
    }
+   
+
+    handleGesture() {
+      if (this.touchendX < this.touchstartX) {
+         this.next();
+         
+       }
+       
+       else if (this.touchendX > this.touchstartX) {
+         this.previous();
+      }
+      else if (this.touchendY == this.touchstartY) {
+         
+      }
+   }
+   
+
 }
 
-buttonNext.onclick = () => slider.next();
-buttonPrev.onclick = () => slider.previous();
-window.addEventListener('resize', setSize)
-document.addEventListener('DOMContentLoaded', getEvents)
+
+function getEvents() {
+   for (let i = 0; i < slider.sliderIndicators.length; i++) {
+      slider.sliderIndicators[i].addEventListener('click', () => {
+         slider.position = -i * slider.width;
+         slider.newPosition(i);
+         slider.n = i;
+     
+      })
+   }
+}
 
 
 let slider = new Slider(width);
+slider.init()
+
 
 function setSize() {
-   sliderList.style.marginLeft = 0;
-   setIndicator(0)
-   newWidth = document.querySelector('.slider__item').clientWidth;
-   n = 0
+   slider.sliderList.style.marginLeft = 0;
+   slider.setIndicator(0)
+   slider.newWidth = document.querySelector('.slider__item').clientWidth;
+   slider.n = 0
    slider = new Slider(newWidth); 
 }
 
-
-
-let touchstartX = 0;
-let touchstartY = 0;
-let touchendX = 0;
-let touchendY = 0;
-
-
-sliderContent.addEventListener('touchstart', function(event) {
-    touchstartX = event.changedTouches[0].screenX;
-    touchstartY = event.changedTouches[0].screenY;
+slider.sliderContent.addEventListener('touchstart', function(event) {
+   slider.touchstartX = event.changedTouches[0].screenX;
+   slider.touchstartY = event.changedTouches[0].screenY;
 }, false);
 
-sliderContent.addEventListener('touchend', function(event) {
-    touchendX = event.changedTouches[0].screenX;
-    touchendY = event.changedTouches[0].screenY;
-    handleGesture();
+slider.sliderContent.addEventListener('touchend', function(event) {
+    slider.touchendX = event.changedTouches[0].screenX;
+    slider.touchendY = event.changedTouches[0].screenY;
+    slider.handleGesture();
 }, false); 
 
-function handleGesture() {
-   if (touchendX < touchstartX) {
-      slider.next();
-      
-    }
-    
-    else if (touchendX > touchstartX) {
-      slider.previous();
-    }
-}
 
 
 
